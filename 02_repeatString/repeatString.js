f5663470010bbe6710bcc1cd4cbef6e65cc35914
travisduplessis <144057401+travisduplessis901@users.noreply.github.com>
2024-01-15 22:03:51 +0200

    Excersie 01 and 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let newString = "";
 
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newString = newString + string;
+        }
+    }else{
+        newString = "ERROR";
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
